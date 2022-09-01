475e211ab30d5cfbdc1bb71b314575fa1140846b
Bethany Murillo <luckyb777@me.com>
2022-09-01 13:15:13 -0700

    adding logic.js file to  Mapping_single_points branch

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -8,14 +8,39 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// want to add a marker using circle() function instead, so edited marker code above
+// skill drill: create dark map with lightyellow circle with black line 
+// indicating 300 meter radius of Central LA
+// couldn't figure out how to create black line? will need to look for more doc later 
+/*
+L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillOpacity: "0.4",
+    fillColor: '#ffffa1'
+ }).addTo(map);
+ */
+
+// Alternatively= can create circle using circleMarker() function 
+ // difference: circleMarker() fcn measures radius in Pixels (circle() measures in meters) 
+ // default radius is set at 10 pixels
+L.circleMarker([34.0522,-118.2437],{
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+    // expected output: circle will be huge at first and then get smaller as you zoom in 
+
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
+    // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
