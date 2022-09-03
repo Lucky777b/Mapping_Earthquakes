7521b0eafabfb4830ad72e0960c47f637e8b0a86
Bethany Murillo <luckyb777@me.com>
2022-09-02 18:16:34 -0700

    add majorAirports.json

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -4,38 +4,7 @@ console.log("working");
 // Create the map object with center at the San Francisco airport.
 let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Add GeoJSON data.
-let sanFranAirport ={
-    "type":"FeatureCollection","features":[{
-        "type":"Feature",
-        "properties":{
-            "id":"3469",
-            "name":"San Francisco International Airport",
-            "city":"San Francisco",
-            "state":"CA",
-            "country":"United States",
-            "faa":"SFO",
-            "icao":"KSFO",
-            "alt":"13",
-            "tz-offset":"-8",
-            "dst":"A",
-            "tz":"America/Los_Angeles"},
-            "geometry":{
-                "type":"Point",
-                "coordinates":[-122.375,37.61899948120117]}}
-        ]};
 
-/*
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    pointToLayer: function(feature, latlng) {
-        console.log(feature);
-        return L.marker(latlng)
-        .bindPopup("<h2>" + feature.properties.city + ", " + feature.properties.state + "</h2><br><h3>" + feature.properties.name + "</h3>");
-    }
-}).addTo(map);
-*/
 
 // Grabbing our GeoJSON data using onEachFeature()
 L.geoJSON(sanFranAirport, {
@@ -44,6 +13,7 @@ L.geoJSON(sanFranAirport, {
         layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2><br><hr><br><h2> Airport Name: " + feature.properties.name + "</h2>");
     }
 }).addTo(map);
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
